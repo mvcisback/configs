a8642dbb849c34c1ed5855bbb6056ddf8000db94
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-12-24 02:27:12 -0600

    added hardware configuration

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./hardware-configuration-nano.nix
     ];
 
   # Use the GRUB 2 boot loader.
